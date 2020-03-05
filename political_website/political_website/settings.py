7097a454299d3d91f2578492bc62fd5bfaa92927
faci2000 <50948467+faci2000@users.noreply.github.com>
2020-03-05 23:51:08 +0100

    static files fix

--- political_website/political_website/settings.py
+++ political_website/political_website/settings.py
@@ -119,14 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = ''
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/    
 
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATIC_URL = '/static/'    
 
-
-STATICFILES_DIRS = (
-
-    ('assets', os.path.join(PROJECT_DIR, '../static')),
-)
-PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = [
+    os.path.join(PROJECT_ROOT, 'static'),
+]
