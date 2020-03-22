9ec16e1a5eacc265d42b6c4bd98a13bd20d78fbc
faci2000 <50948467+faci2000@users.noreply.github.com>
2020-03-22 22:28:49 +0100

    changed scroll script

--- articles/static/articles/js/scrollto.js
+++ articles/static/articles/js/scrollto.js
@@ -1,6 +1,18 @@
-$(document).on('click', 'a', function(event){
-    //event.preventDefault();
-    $('body').animate({
-        scrollTop: $($.attr(this, 'href')).offset().top
-    }, 500);
-});
+var hashTagActive = "";
+    $(".scroll").on("click touchstart" , function (event) {
+        if(hashTagActive != this.hash) { //this will prevent if the user click several times the same link to freeze the scroll.
+            event.preventDefault();
+            //calculate destination place
+            var dest = 0;
+            if ($(this.hash).offset().top > $(document).height() - $(window).height()) {
+                dest = $(document).height() - $(window).height();
+            } else {
+                dest = $(this.hash).offset().top;
+            }
+            //go to destination
+            $('html,body').animate({
+                scrollTop: dest
+            }, 2000, 'swing');
+            hashTagActive = this.hash;
+        }
+    });
